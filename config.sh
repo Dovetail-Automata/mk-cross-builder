6e9cabf0bb7e87084e2720dd27ebe7c0c312b11b
John Morris <john@zultron.com>
2017-09-26 16:21:03 -0500

    Mega patch:  Rework Docker hub build scheme; update everything else
    The old Docker hub scheme was inconvenient.  With one `Dockerfile` per
    branch, it was annoying to update branches after changes, and it was
    confusing for others to use.
    
    This patch collapses everything into the master branch, templating
    `Dockerfile` into `dockerfiles/Dockerfile.$TAG`.
    
    - Separate configuration into `config.sh`
    - Update `mk-cross-builder.sh` to do the templating
      - Merged in from `update-templates.sh`
    - Dockerfile:
      - Build fixes
      - Make base image configurable
    - README:  update build instructions

--- config.sh
+++ config.sh
@@ -1,19 +1,16 @@
-#!/bin/bash -e
-# Template `dockerfiles/Dockerfile.$DISTRO.$DEBIAN_ARCH` for each tag
-
 ###################################################################
-# Tag branch settings
+# Tag settings
 
-BASE_BRANCH="master"
 TAGS="amd64_8 armhf_8 i386_8 raspbian_8 amd64_9 armhf_9 i386_9 raspbian_9"
 ATTRS="DEBIAN_ARCH SYS_ROOT HOST_MULTIARCH DISTRO_CODENAME
-       DISTRO_VER EXTRA_FLAGS"
+       BASE_IMAGE DISTRO_VER EXTRA_FLAGS"
 
 declare -A SETTINGS_amd64_8=(
     [DEBIAN_ARCH]="amd64"
     [SYS_ROOT]=
     [HOST_MULTIARCH]="x86_64-linux-gnu"
     [DISTRO_CODENAME]="jessie"
+    [BASE_IMAGE]="debian:jessie"
     [DISTRO_VER]="8"
     [EXTRA_FLAGS]=
 )
@@ -22,6 +19,7 @@ declare -A SETTINGS_armhf_8=(
     [SYS_ROOT]="/sysroot/armhf"
     [HOST_MULTIARCH]="arm-linux-gnueabihf"
     [DISTRO_CODENAME]="jessie"
+    [BASE_IMAGE]="debian:jessie"
     [DISTRO_VER]="8"
     [EXTRA_FLAGS]=
 )
@@ -30,6 +28,7 @@ declare -A SETTINGS_i386_8=(
     [SYS_ROOT]="/sysroot/i386"
     [HOST_MULTIARCH]="i386-linux-gnu"
     [DISTRO_CODENAME]="jessie"
+    [BASE_IMAGE]="debian:jessie"
     [DISTRO_VER]="8"
     [EXTRA_FLAGS]="-m32"
 )
@@ -38,6 +37,7 @@ declare -A SETTINGS_raspbian_8=(
     [SYS_ROOT]="/sysroot/rpi"
     [HOST_MULTIARCH]="arm-linux-gnueabihf"
     [DISTRO_CODENAME]="raspbian"
+    [BASE_IMAGE]="debian:jessie"
     [DISTRO_VER]="8"
     [EXTRA_FLAGS]=
 )
@@ -47,6 +47,7 @@ declare -A SETTINGS_amd64_9=(
     [SYS_ROOT]=
     [HOST_MULTIARCH]="x86_64-linux-gnu"
     [DISTRO_CODENAME]="stretch"
+    [BASE_IMAGE]="debian:stretch"
     [DISTRO_VER]="9"
     [EXTRA_FLAGS]=
 )
@@ -55,6 +56,7 @@ declare -A SETTINGS_armhf_9=(
     [SYS_ROOT]="/sysroot/armhf"
     [HOST_MULTIARCH]="arm-linux-gnueabihf"
     [DISTRO_CODENAME]="stretch"
+    [BASE_IMAGE]="debian:stretch"
     [DISTRO_VER]="9"
     [EXTRA_FLAGS]=
 )
@@ -63,6 +65,7 @@ declare -A SETTINGS_i386_9=(
     [SYS_ROOT]="/sysroot/i386"
     [HOST_MULTIARCH]="i386-linux-gnu"
     [DISTRO_CODENAME]="stretch"
+    [BASE_IMAGE]="debian:stretch"
     [DISTRO_VER]="9"
     [EXTRA_FLAGS]="-m32"
 )
@@ -71,28 +74,7 @@ declare -A SETTINGS_raspbian_9=(
     [SYS_ROOT]="/sysroot/rpi"
     [HOST_MULTIARCH]="arm-linux-gnueabihf"
     [DISTRO_CODENAME]="raspbian"
+    [BASE_IMAGE]="debian:stretch"
     [DISTRO_VER]="9"
     [EXTRA_FLAGS]=
 )
-
-
-TOPDIR="$(dirname $0)"
-DOCKERFILEDIR="$TOPDIR/dockerfiles"
-
-template_dockerfile() {
-    # Customize Dockerfile for a tag
-    TAG=$1
-    declare -a substitutions=()
-    for KEY in $ATTRS; do
-	eval VAL="\${SETTINGS_$TAG[$KEY]}"
-	substitutions+=( "-e" "s,@$KEY@,$VAL," )
-    done
-    # eval DEBIAN_ARCH="\${SETTINGS_$TAG[DEBIAN_ARCH]}"
-    sed Dockerfile "${substitutions[@]}" \
-	> "$DOCKERFILEDIR/Dockerfile.$TAG"
-}
-
-mkdir -p "$DOCKERFILEDIR"
-for TAG in $TAGS; do
-    template_dockerfile $TAG
-done
